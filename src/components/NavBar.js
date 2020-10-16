86167bb4f5145188c1ca13cbb2d4e0f5fa889f8e
William Parra <w3webservices.net@gmail.com>
2020-10-16 17:23:20 -0400

    se creo el nav mas estilizado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/navbar.css'
 import { Link } from 'react-router-dom'
 
 function NavBar() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const openClose = () => {
+        if (isOpen) {
+            setIsOpen(false)
+        }
+        else {
+            setIsOpen(true)
+        }
+
+        console.log(isOpen)
+    }
+
+    const isOpenAct = () => {
+        var setOpenClass = "isOpen"
+        if (isOpen) {
+            return setOpenClass
+        }
+        else {
+            return ""
+        }
+    }
+
     return (
         <div className="nav_element">
             <div className="nav-container">
@@ -17,7 +40,7 @@ function NavBar() {
                             <p className="brand-title">Home</p>
                         </Link>
                     </div>
-                    <div className="navbar-options">
+                    <div className={`navbar-options ${isOpenAct()}`} onClick={() => (setIsOpen(false))}>
                         <Link to="/articulos-para-streaming">
                             <p>Productos</p>
                         </Link>
@@ -27,6 +50,10 @@ function NavBar() {
                         <Link to="/guia-completa-para-hacer-streaming-gamer-2020">
                             <p>Guia de Streaming</p>
                         </Link>
+                    </div>                    
+                    <div className="phone-nav-element" onClick={openClose}>
+                        {isOpen && <i className="fas fa-arrow-circle-up"></i>}
+                        {!isOpen && <i className="fas fa-arrow-circle-down"></i>}
                     </div>
                 </nav>
             </div>
