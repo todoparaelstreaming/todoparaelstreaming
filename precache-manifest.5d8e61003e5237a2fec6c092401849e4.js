1b33014b2ee1446261a80aada96976f32afae5d8
William Parra <w3webservices.net@gmail.com>
2020-10-21 10:21:15 -0400

    Updates

---
+++ precache-manifest.5d8e61003e5237a2fec6c092401849e4.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfd08463c6eab0ca2dce269fb434a17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda4443bbfb23af77f7c",
+    "url": "/static/css/main.9f26b6c5.chunk.css"
+  },
+  {
+    "revision": "0f9f0fe810700771fc24",
+    "url": "/static/js/2.dad9004c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dad9004c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda4443bbfb23af77f7c",
+    "url": "/static/js/main.6f06f547.chunk.js"
+  },
+  {
+    "revision": "e296decc78b2f62def48",
+    "url": "/static/js/runtime-main.cc15af7f.js"
+  },
+  {
+    "revision": "30d7195eb55a81b0b1dd1c046c47dad8",
+    "url": "/static/media/AS-resultado.30d7195e.jpg"
+  },
+  {
+    "revision": "02b696332ef79b9e7bdfcc15d1938c15",
+    "url": "/static/media/AS-resultadoWebp.02b69633.webp"
+  },
+  {
+    "revision": "2d8aa52f4ea9819dc2e5cab888381cb3",
+    "url": "/static/media/ASpaso1.2d8aa52f.jpg"
+  },
+  {
+    "revision": "f79f8bd66744d2aeaa00731a34581e7a",
+    "url": "/static/media/ASpaso1Webp.f79f8bd6.webp"
+  },
+  {
+    "revision": "11981a8c4b8cd6ae9d9b473ba2051d88",
+    "url": "/static/media/ASpaso2.11981a8c.jpg"
+  },
+  {
+    "revision": "086f2fd01335151ab8aeb4b22bf434b4",
+    "url": "/static/media/ASpaso2Webp.086f2fd0.webp"
+  },
+  {
+    "revision": "0cb42d8bb2c5d6979e3be0a33da0378d",
+    "url": "/static/media/ASpaso3.0cb42d8b.jpg"
+  },
+  {
+    "revision": "72399b2d49dc53bf79ea2cd4c1c2f803",
+    "url": "/static/media/ASpaso3Webp.72399b2d.webp"
+  },
+  {
+    "revision": "0cc88dcefc8b3a1b2dc47a6ef19b159f",
+    "url": "/static/media/SeAAnwbmp.0cc88dce.webp"
+  },
+  {
+    "revision": "9794ed3af90eec21ad437341bc30bb91",
+    "url": "/static/media/avermediaFull.9794ed3a.webp"
+  },
+  {
+    "revision": "fcc5bb063f5a8c6efbba2c1f8812ec06",
+    "url": "/static/media/comohacerstreaming1.fcc5bb06.JPG"
+  },
+  {
+    "revision": "e1ee85e0c235e8e218ddf2c261e22a72",
+    "url": "/static/media/comohacerstreaming1webp.e1ee85e0.webp"
+  },
+  {
+    "revision": "5c641aaafaaebe873144752cb897a5ad",
+    "url": "/static/media/elgatowebp.5c641aaa.webp"
+  },
+  {
+    "revision": "01d60b0a0d62f7b0768beff6c2604969",
+    "url": "/static/media/linkstablereducejpg.01d60b0a.webp"
+  },
+  {
+    "revision": "623bc50de0d8a69321a259cabe0cb93d",
+    "url": "/static/media/miraboxrecudewbpm.623bc50d.webp"
+  },
+  {
+    "revision": "0c6178902f128da58bead8e340933013",
+    "url": "/static/media/openBroadcasterJPG.0c617890.JPG"
+  },
+  {
+    "revision": "1d9009a4d73e0864cab46fad0625010f",
+    "url": "/static/media/openBroadcasterWebp.1d9009a4.webp"
+  },
+  {
+    "revision": "99bfdfae76e23f46bbcf6c5d08255d94",
+    "url": "/static/media/paso1.99bfdfae.png"
+  },
+  {
+    "revision": "35c681b77cb2c1aa343dfec2a5f82fa0",
+    "url": "/static/media/paso1webp.35c681b7.webp"
+  },
+  {
+    "revision": "023ebffe979e2ccd5475bbf6295b9d88",
+    "url": "/static/media/paso2.023ebffe.png"
+  },
+  {
+    "revision": "6dde2e17ac87314449e1c160be5c335e",
+    "url": "/static/media/paso2webp.6dde2e17.webp"
+  },
+  {
+    "revision": "4f565b5327db56cf6b4d300cb314cab2",
+    "url": "/static/media/paso3.4f565b53.PNG"
+  },
+  {
+    "revision": "a5edc512d0d464316ff0824797abe43a",
+    "url": "/static/media/paso3webp.a5edc512.webp"
+  },
+  {
+    "revision": "375247d69f63bbb49c05e614a1fe3167",
+    "url": "/static/media/paso4.375247d6.PNG"
+  },
+  {
+    "revision": "a3afd34fa43f4cea921695dd2d2c14dd",
+    "url": "/static/media/paso4webp.a3afd34f.webp"
+  },
+  {
+    "revision": "bf34875ae5f53e609bd7045815b8516c",
+    "url": "/static/media/pengoReducewbpm.bf34875a.webp"
+  },
+  {
+    "revision": "e0a215be36dca3258f6c0a6da9e49e21",
+    "url": "/static/media/qgeemwbmp.e0a215be.webp"
+  },
+  {
+    "revision": "ce32f77e3bc725188ee2d5b6e4e5fb61",
+    "url": "/static/media/razerwebpReduce.ce32f77e.webp"
+  },
+  {
+    "revision": "238c07d4700acef5a15a3fbaf2f91338",
+    "url": "/static/media/streamLabsCompressWebp.238c07d4.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todoparaelstreaming/precache-manifest.4addebfc61138ddfef2f89ce90207a17.js"
+  "/precache-manifest.5d8e61003e5237a2fec6c092401849e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todoparaelstreaming/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
